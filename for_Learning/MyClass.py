df329856f75bc1b082f013a83adfac233e2cd327
gvkuzmen <gvkuzmen@icloud.com>
2021-03-26 11:40:09 +0300

    new project

--- for_Learning/MyClass.py
+++ for_Learning/MyClass.py
@@ -81,12 +81,58 @@ buf.add(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
 buf.get_current_part()
 
 
-#Второй вариант решения
-#Второй вариант решения
-#Второй вариант решения
-#Второй вариант решения
-#Второй вариант решения#Второй вариант решения
+
+# Второй вариант решения
+
+class Buffer:
+    def __init__(self):
+        self.part = []
+
+    def add(self, *a):
+        for i in a:
+            self.part.append(i)
+            if len(self.part) == 5:
+                print(sum(self.part))
+                self.part.clear()
+
+    def get_current_part(self):
+        return self.part
 
 
+buf = Buffer()
+buf.add(1, 2, 3)
+buf.get_current_part()
+buf.add(4, 5, 6)
+buf.get_current_part()
+buf.add(7, 8, 9, 10)
+buf.get_current_part()
+buf.add(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+buf.get_current_part()
 
 
+# Третий вариант решения
+
+
+class Buffer:
+    def __init__(self):
+        self.lst = []
+
+    def add(self, *a):
+        self.lst += a
+        while len(self.lst) >= 5:
+            print(sum(self.lst[:5]))
+            del (self.lst[:5])
+
+    def get_current_part(self):
+        return self.lst
+        # print(*self.lst) # снять комментарий для самоконтроля
+
+buf = Buffer()
+buf.add(1, 2, 3)
+buf.get_current_part()
+buf.add(4, 5, 6)
+buf.get_current_part()
+buf.add(7, 8, 9, 10)
+buf.get_current_part()
+buf.add(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+buf.get_current_part()
